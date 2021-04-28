e25e1c621dae3aac1d5d6d2a60a8e2216ef60257
lvxl <xl.lv@fugro.com>
2021-04-28 17:10:48 +0800

    Test2

--- Plugin.Test2/Class1.cs
+++ Plugin.Test2/Class1.cs
@@ -2,7 +2,11 @@
 
 namespace Plugin.Test2
 {
-    public class Class1
+    public class Class1 : Plugin.Interface.NDInterfaces
     {
+        public void TestMethod()
+        {
+            Console.WriteLine("This is Test2.");
+        }
     }
 }
